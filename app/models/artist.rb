2d3502b258f34332cf7f179785f0c49dfc86e60d
Jessica Antunes <jessantunes@protonmail.com>
2021-04-01 20:38:11 -0400

    commit tree 7a3232af002b81614c6bc7e7ea9728761036ceab

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,13 +4,19 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+   song = Song.where(artist: self).first
+   song.genre
   end
 
   def song_count
     #return the number of songs associated with the artist
+    Song.where(artist: self).count
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    Song.where(artist: self).map do |song| 
+      song.genre
+    end.count
   end
 end
